4581eea58dea98c6dafddf1d1b2c91785d8d312e
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-01-08 14:52:02 -0500

    recheck missing forecasts for 2024

--- run_forecasts.R
+++ run_forecasts.R
@@ -51,90 +51,81 @@ message('submit fARIMA')
 neon4cast::submit(forecast_file = file.path('Forecasts', fARIMA_file),
                   ask = F)
 
-# # Check for missing forecasts
-# message('Checking for missing ARIMA forecasts')
-# 
-# challenge_model_name <- 'fARIMA'
-# 
-# 
-# # Dates of forecasts 
-# today <- paste(Sys.Date() - days(2), '00:00:00')
-# this_year <- data.frame(date = as.character(paste0(seq.Date(as_date('2023-01-01'), to = as_date(today), by = 'day'), ' 00:00:00')),
-#                         exists = NA)
-# # issue resolved on 9 Oct 2023
+# Check for missing forecasts
+message('Checking for missing ARIMA forecasts')
+
+challenge_model_name <- 'fARIMA'
+
+
+# Dates of forecasts
+today <- paste(Sys.Date() - days(2), '00:00:00')
+this_year <- data.frame(date = as.character(paste0(seq.Date(as_date('2024-01-01'), 
+                                                            to = as_date(today),
+                                                            by = 'day'), 
+                                                   ' 00:00:00')),
+                        exists = NA)
+# issue resolved on 9 Oct 2023
 # end <- as_date('2023-10-09')
-# # what forecasts have already been submitted?
-# challenge_s3_region <- "data"
-# challenge_s3_endpoint <- "ecoforecast.org"
-# 
-# Sys.unsetenv("AWS_ACCESS_KEY_ID")
-# Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
-# Sys.unsetenv("AWS_DEFAULT_REGION")
-# Sys.unsetenv("AWS_S3_ENDPOINT")
-# 
-# # is that file present in the bucket?
-# for (i in 1:nrow(this_year)) {
-#   forecast_file <- paste0('aquatics-', as_date(this_year$date[i]), '-', challenge_model_name, '.csv.gz')
-#   
-#   this_year$exists[i] <- suppressMessages(aws.s3::object_exists(object = file.path("raw", 'aquatics', forecast_file),
-#                                                                 bucket = "neon4cast-forecasts",
-#                                                                 region = challenge_s3_region,
-#                                                                 base_url = challenge_s3_endpoint))
-#   
-#   # if it is present, has it been submitted recently?
-#   if (this_year$exists[i]) {
-#     modified <- attr(suppressMessages(aws.s3::head_object(object = file.path("raw", 'aquatics', forecast_file),
-#                                                           bucket = "neon4cast-forecasts",
-#                                                           region = challenge_s3_region,
-#                                                           base_url = challenge_s3_endpoint)), 
-#                      "last-modified")
-#     
-#     this_year$already_rerun[i] <- ifelse(parse_date_time(gsub('GMT', '', str_split_1(modified, ', ')[2]),
-#                                                          orders = "%d %b %Y %H:%M:%S") > end, 
-#                                          T, F)
-#   }
-# }
-# 
-# # which dates do you need to generate forecasts for?
-# # those that are missing or haven't been submitted
-# missed_dates <- this_year |> 
-#   filter(!(exists == T &  already_rerun == T)) |> 
-#   pull(date) |> 
-#   as_date()
-# 
-# 
-# 
-# for (i in 1:length(missed_dates)) {
-#   
-#   forecast_date <- missed_dates[i]
-#   
-#   # Download the NOAA data needed
-#   noaa_data <- download_noaa(sites = sites,
-#                              forecast_date = forecast_date)
-#   
-#   if (is.list(noaa_data)) {
-#     # Generate the forecasts
-#     fARIMA_file <- generate_fARIMA(team_name = 'fARIMA',
-#                                    sites = sites,
-#                                    var = 'temperature',
-#                                    theme = 'aquatics',
-#                                    forecast_date = forecast_date,
-#                                    n = 200, 
-#                                    h = 30, 
-#                                    target_url = target_url, 
-#                                    noaa_future = noaa_data$future, 
-#                                    noaa_past = noaa_data$past)
-#     # Submit forecast!
-#     message('submitting missing fARIMA forecast')
-#     neon4cast::submit(forecast_file = file.path('Forecasts', fARIMA_file),
-#                       ask = F)
-#   } else {
-#     message('Cannot submit forecast for this date')
-#   }
-#   
-#   
-#   
-# }
+
+# Get all the submissions 
+submissions <- aws.s3::get_bucket_df("bio230014-bucket01", 
+                                     prefix = "challenges/forecasts/raw",
+                                     region = "sdsc",
+                                     base_url = "osn.xsede.org",
+                                     max = Inf)
+
+Sys.unsetenv("AWS_ACCESS_KEY_ID")
+Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
+Sys.unsetenv("AWS_DEFAULT_REGION")
+Sys.unsetenv("AWS_S3_ENDPOINT")
+
+# is that file present in the bucket?
+for (i in 1:nrow(this_year)) {
+  forecast_file <- paste0('aquatics-', as_date(this_year$date[i]), '-', challenge_model_name, '.csv.gz')
+
+  this_year$exists[i] <- nrow(dplyr::filter(submissions, stringr::str_detect(Key, forecast_file))) > 0
+}
+
+# which dates do you need to generate forecasts for?
+# those that are missing or haven't been submitted
+missed_dates <- this_year |>
+  filter(exists == F) |>
+  pull(date) |>
+  as_date()
+
+
+
+for (i in 1:length(missed_dates)) {
+
+  forecast_date <- missed_dates[i]
+
+  # Download the NOAA data needed
+  noaa_data <- download_noaa(sites = sites,
+                             forecast_date = forecast_date)
+
+  if (is.list(noaa_data)) {
+    # Generate the forecasts
+    fARIMA_file <- generate_fARIMA(team_name = 'fARIMA',
+                                   sites = sites,
+                                   var = 'temperature',
+                                   theme = 'aquatics',
+                                   forecast_date = forecast_date,
+                                   n = 200,
+                                   h = 30,
+                                   target_url = target_url,
+                                   noaa_future = noaa_data$future,
+                                   noaa_past = noaa_data$past)
+    # Submit forecast!
+    message('submitting missing fARIMA forecast')
+    neon4cast::submit(forecast_file = file.path('Forecasts', fARIMA_file),
+                      ask = F)
+  } else {
+    message('Cannot submit forecast for this date')
+  }
+
+
+
+}
 
 #----------------------------------------------------------#
 
@@ -160,86 +151,68 @@ message('submit TSLM forecast')
 neon4cast::submit(forecast_file = file.path('Forecasts', fTSLM_file),
                   ask = F)
 
-# # Check for missing forecasts
-# message('Checking for missing TSLM forecasts')
-# 
-# challenge_model_name <- 'fTSLM_lag'
-# 
-# 
-# # Dates of forecasts 
-# today <- paste(Sys.Date() - days(2), '00:00:00')
-# this_year <- data.frame(date = as.character(paste0(seq.Date(as_date('2023-01-01'), to = as_date(today), by = 'day'), ' 00:00:00')),
-#                         exists = NA)
-# 
-# # what forecasts have already been submitted?
-# challenge_s3_region <- "data"
-# challenge_s3_endpoint <- "ecoforecast.org"
-# 
-# Sys.unsetenv("AWS_ACCESS_KEY_ID")
-# Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
-# Sys.unsetenv("AWS_DEFAULT_REGION")
-# Sys.unsetenv("AWS_S3_ENDPOINT")
-# 
-# # is that file present in the bucket?
-# for (i in 1:nrow(this_year)) {
-#   forecast_file <- paste0('aquatics-', as_date(this_year$date[i]), '-', challenge_model_name, '.csv.gz')
-#   
-#   this_year$exists[i] <- suppressMessages(aws.s3::object_exists(object = file.path("raw", 'aquatics', forecast_file),
-#                                                                 bucket = "neon4cast-forecasts",
-#                                                                 region = challenge_s3_region,
-#                                                                 base_url = challenge_s3_endpoint))
-#   
-#   # if it is present, has it been submitted recently?
-#   if (this_year$exists[i]) {
-#     modified <- attr(suppressMessages(aws.s3::head_object(object = file.path("raw", 'aquatics', forecast_file),
-#                                                           bucket = "neon4cast-forecasts",
-#                                                           region = challenge_s3_region,
-#                                                           base_url = challenge_s3_endpoint)), 
-#                      "last-modified")
-#     
-#     this_year$already_rerun[i] <- ifelse(parse_date_time(gsub('GMT', '', str_split_1(modified, ', ')[2]),
-#                                                          orders = "%d %b %Y %H:%M:%S") > end, 
-#                                          T, F)
-#   }
-# }
-# 
-# # which dates do you need to generate forecasts for?
-# # those that are missing or haven't been submitted
-# missed_dates <- this_year |> 
-#   filter(!(exists == T &  already_rerun == T)) |> 
-#   pull(date) |> 
-#   as_date()
-# 
-# 
-# for (i in 1:length(missed_dates)) {
-#   
-#   forecast_date <- missed_dates[i]
-#   
-#   # Download the NOAA data needed
-#   noaa_data <- download_noaa(sites = sites,
-#                              forecast_date = forecast_date)
-#   
-#   if (is.list(noaa_data)){
-#     # Generate the forecasts
-#     fTSLM_file <- generate_fTSLM_lag(team_name = 'fTSLM_lag',
-#                                      sites = sites,
-#                                      var = 'temperature',
-#                                      theme = 'aquatics',
-#                                      forecast_date = forecast_date,
-#                                      n = 200, 
-#                                      h = 30, 
-#                                      target_url = target_url, 
-#                                      noaa_future = noaa_data$future, 
-#                                      noaa_past = noaa_data$past)
-#     # Submit forecast!
-#     message('submitting missing TSLM forecast')
-#     neon4cast::submit(forecast_file = file.path('Forecasts', fTSLM_file),
-#                       ask = F)
-#   } else {
-#     message('Cannot submit forecast for this date')
-#   }
-#   
-#   
-#   
-# }
+# Check for missing forecasts
+message('Checking for missing TSLM forecasts')
+
+challenge_model_name <- 'fTSLM_lag'
+
+
+# Dates of forecasts
+today <- paste(Sys.Date() - days(2), '00:00:00')
+this_year <- data.frame(date = as.character(paste0(seq.Date(as_date('2024-01-01'), to = as_date(today), by = 'day'), ' 00:00:00')),
+                        exists = NA)
+
+
+Sys.unsetenv("AWS_ACCESS_KEY_ID")
+Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
+Sys.unsetenv("AWS_DEFAULT_REGION")
+Sys.unsetenv("AWS_S3_ENDPOINT")
+
+# is that file present in the bucket?
+for (i in 1:nrow(this_year)) {
+  forecast_file <- paste0('aquatics-', as_date(this_year$date[i]), '-', challenge_model_name, '.csv.gz')
+
+  this_year$exists[i] <- nrow(dplyr::filter(submissions, stringr::str_detect(Key, forecast_file))) > 0
+
+}
+
+# which dates do you need to generate forecasts for?
+# those that are missing or haven't been submitted
+missed_dates <- this_year |>
+  filter(exists == F) |>
+  pull(date) |>
+  as_date()
+
+
+for (i in 1:length(missed_dates)) {
+
+  forecast_date <- missed_dates[i]
+
+  # Download the NOAA data needed
+  noaa_data <- download_noaa(sites = sites,
+                             forecast_date = forecast_date)
+
+  if (is.list(noaa_data)){
+    # Generate the forecasts
+    fTSLM_file <- generate_fTSLM_lag(team_name = 'fTSLM_lag',
+                                     sites = sites,
+                                     var = 'temperature',
+                                     theme = 'aquatics',
+                                     forecast_date = forecast_date,
+                                     n = 200,
+                                     h = 30,
+                                     target_url = target_url,
+                                     noaa_future = noaa_data$future,
+                                     noaa_past = noaa_data$past)
+    # Submit forecast!
+    message('submitting missing TSLM forecast')
+    neon4cast::submit(forecast_file = file.path('Forecasts', fTSLM_file),
+                      ask = F)
+  } else {
+    message('Cannot submit forecast for this date')
+  }
+
+
+
+}
 #----------------------------------------------------------------#
