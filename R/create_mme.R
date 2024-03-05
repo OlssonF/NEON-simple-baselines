d99c1975ff624e9dbb28d5abce94d442498c97ce
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-03-05 13:51:01 -0500

    make it less spiky

--- R/create_mme.R
+++ R/create_mme.R
@@ -53,7 +53,8 @@ create_mme <- function(forecast_models, # vector of list of model names
           
           group_by(site_id, datetime, model_id) |> 
           # sample from the distribution based on the mean and sd
-          reframe(prediction = rnorm(sample, mean = mu, sd = sigma)) |> 
+          reframe(prediction = rnorm(sample, mean = mu, sd = sigma)) |>
+          arrange(datetime, site_id, prediction) |> 
           group_by(site_id, datetime) |> 
           # parameter value needs to be character
           mutate(parameter = as.character(row_number()),
